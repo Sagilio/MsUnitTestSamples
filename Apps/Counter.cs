6bb5a88fe111c7a9b4ffbe524bbcb1a51c48f5f5
Sagilio <Sagilio@outlook.com>
2020-04-24 15:37:38 +0800

    添加项目文件。

---
+++ Apps/Counter.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace MsUnitTestSample.Apps
+{
+    public class Counter
+    {
+        public ValueTask<int> Add(params int[] num)
+        {
+            return new ValueTask<int>(num.Sum());
+        }
+
+        public ValueTask<int> Count(params int[] num)
+        {
+            return new ValueTask<int>(num.Length - 1);
+        }
+    }
+}
---
+++ CountTest.cs
@@ -0,0 +1,34 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MsUnitTestSample.Apps;
+using System.Threading.Tasks;
+
+namespace MsUnitTestSamples
+{
+    [TestClass]
+    public class CountTest
+    {
+        private Counter Counter { get; } = new Counter();
+
+        [TestMethod]
+        public async Task ShouldAdd()
+        {
+            const int i = 5;
+            const int j = 6;
+            const int result = 11;
+
+            var output = await Counter.Add(i, j);
+            Assert.AreEqual(result, output);
+        }
+
+        [TestMethod]
+        public async Task ShouldCount()
+        {
+            const int i = 5;
+            const int j = 6;
+            const int result = 2;
+
+            var output = await Counter.Count(i,j);
+            Assert.AreEqual(result, output);
+        }
+    }
+}
